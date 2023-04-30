38f7c335b013f20d598bf0099b487937ff993685
Stephen Monaco <stevemonaco@hotmail.com>
2023-04-30 17:12:17 -0400

    Update to Avalonia 11-preview7

--- RagnaRoute/Services/ClipboardService.cs
+++ RagnaRoute/Services/ClipboardService.cs
@@ -1,4 +1,7 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input.Platform;
 using System.Threading.Tasks;
 
 namespace RagnaRoute.Services;
@@ -12,12 +15,22 @@ public class ClipboardService : IClipboardService
 {
     public async Task<bool> CopyTextAsync(string text)
     {
-        if (Application.Current?.Clipboard is not null)
+        if (GetClipboard() is { } clipboard)
         {
-            await Application.Current.Clipboard.SetTextAsync(text);
+            await clipboard.SetTextAsync(text);
             return true;
         }
 
         return false;
+    }
+
+    private static IClipboard? GetClipboard()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
+        {
+            if (TopLevel.GetTopLevel(lifetime.MainWindow)?.Clipboard is { } clipboard)
+                return clipboard;
+        }
+        return null;
     }
 }
